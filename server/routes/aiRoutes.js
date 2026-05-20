74849b8cd6d3f5e5045b20d1ece4476b401963f9
Sahil-husen <sahilhussainsahil76@gmail.com>
2026-05-21 00:05:08 +0530

    AI routes added

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,4 +1,11 @@
+import express from 'express'
+const router = express.Router();
+import protectedMiddleware from '../middleware/authMiddleware.js'
+// import aiController from '../controllers/aiController.js'
+import {generateEmail,getHistory} from '../controllers/aiController.js'
 
 
+router.post("/generate-email",protectedMiddleware,generateEmail)
+router.get("/history",protectedMiddleware,getHistory)
 
-// export default aiRoutes;
+export default router;
