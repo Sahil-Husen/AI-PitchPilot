2bbeb5f2c122dbe2875f7ee3b022414537338614
Sahil-husen <sahilhussainsahil76@gmail.com>
2026-05-21 23:03:23 +0530

    frontend issue resolved

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { AuthProvider } from './context/AuthContext'
+import { AuthProvider } from './context/authContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
