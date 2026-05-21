380bffb477e92e8976c763b02955052c9b38e690
Sahil-husen <sahilhussainsahil76@gmail.com>
2026-05-21 21:40:54 +0530

    protected routes along with some components added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-// import { useAuth } from "./context/authContext";
-import { Toaster } from 'react-hot-toast';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { useAuth } from "./context/authContext";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import VerifyOtp from "./pages/VerifyOtp";
+import Dashboard from "./pages/Dashboard";
+import Layout from "./components/Layout";
 
 function App() {
-  // const { user, loading } = useAuth();
+  const { user, loading } = useAuth();
 
   return (
     <BrowserRouter>
@@ -14,6 +18,14 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/verify-otp" element={<VerifyOtp />} />
+        <Route
+          path="/dashboard"
+          element={user ? <Layout /> : <Navigate to="/login" />}
+        >
+          <Route index  element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
