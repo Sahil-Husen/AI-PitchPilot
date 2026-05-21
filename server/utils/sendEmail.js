82ce0015f30853366503eaa73a70a012d8b14413
Sahil-husen <sahilhussainsahil76@gmail.com>
2026-05-22 01:55:34 +0530

    fix: trust proxy for render deployment

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -7,7 +7,9 @@ const sendEmail = async (options) => {
     }
 
     const transporter = nodemailer.createTransport({
-      service: "Gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
@@ -24,7 +26,6 @@ const sendEmail = async (options) => {
 
     await transporter.sendMail(mailOptions);
     // console.log(`Email sent successfully to ${options.to}`);
-
   } catch (error) {
     console.error("Error sending email:", error.message);
     throw error;
